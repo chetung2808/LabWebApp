409285f048322ae938effc0acc0d41088a4fe84b
chetung2808 <58966385+chetung2808@users.noreply.github.com>
2022-12-08 17:42:18 +0700

    add Ex1

---
+++ HW5/Ex1/backend/index.js
@@ -0,0 +1,32 @@
+
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const videoroute = require('./model/shortVideoModel')
+
+
+  
+
+dotenv.config()          
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected!")).catch((err) => console.log(err))
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('BACKEND IS RUNNING')
+  })
+
+app.use("/api", videoroute)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log("Backend server is running!")
+}) 
